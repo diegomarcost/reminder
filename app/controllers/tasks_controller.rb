a00a0f1ee8599f8f60294cc527df2ee968b026e4
Diego Marcos <dmarcos@concentrico.com.mx>
2013-04-13 15:29:19 -0500

    Create, edit, update, destroy y filtros

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,7 +1,10 @@
 class TasksController < ApplicationController
 
+	before_filter :find_task,
+    :only => [:show, :edit, :update, :destroy]
+
 	def index
-		@tasks = Task.all	
+		redirect_to action: 'new'
 	end
 
 	def new
@@ -9,9 +12,39 @@ class TasksController < ApplicationController
 		@tasks = Task.all	
 	end
 
-	def create
-		
+	def show
 		
 	end
+
+	def create
+      @task = Task.new(params[:task])
+      if @task.save
+            redirect_to action: 'new'
+      else
+            redirect_to action: 'new'
+      end
+   end
+
+   	def edit
+
+   	end
+
+   	def update
+   		if @task.update_attributes(params[:task])
+   			redirect_to action: 'show'
+   		else
+   			redirect_to action: 'edit'
+   		end
+   	end
+
+   def destroy
+   		@task.destroy
+   		redirect_to action: 'new'	
+   end
+
+   private
+	    def find_task
+	      @task = Task.find(params[:id])
+	    end
 
 end
--- app/models/task.rb
+++ app/models/task.rb
@@ -1,3 +1,5 @@
 class Task < ActiveRecord::Base
   attr_accessible :date, :days_before, :description, :title
+
+  validates_presence_of :date, :days_before, :description, :title 
 end
